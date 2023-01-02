ed5a11395d237f90636f0f15c5f48372c8771148
Siphesihle Mhlongo <sihlecodes@gmail.com>
2023-01-02 20:39:40 +0200

    Clamped positioner to lancher range radius

--- Positioner.gd
+++ Positioner.gd
@@ -4,3 +4,18 @@ extends Point
 class_name Positioner
 
 @export var connector_color: Color = Color.CORNFLOWER_BLUE
+
+func as_relative(angle:float, full_rotation = 2 * PI):
+	angle = fmod(angle, full_rotation)
+
+	if (angle < 0):
+		return full_rotation + angle
+	return angle
+
+func _process(delta: float) -> void:
+	var circumference_point = position.normalized() * get_parent().range_radius
+
+	position.x = clamp(position.x, min(0, circumference_point.x), max(0, circumference_point.x))
+	position.y = clamp(position.y, min(0, circumference_point.y), max(0, circumference_point.y))
+
+	super._process(delta)
