1d8c427e06d96401e890547d76a4eb68b4738507
Siphesihle Mhlongo <sihlecodes@gmail.com>
2023-01-11 22:05:51 +0200

    Ground now infinitely pans and scales
    I still need to fix the error with the clamp of path renderer

--- source/Ground.gd
+++ source/Ground.gd
@@ -1,3 +1,18 @@
+@tool
 extends Line2D
 
 class_name Ground
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	var camera = $"../camera"
+	var window_size = get_viewport_rect().size
+
+	var visible_size = window_size / camera.zoom
+
+	var start_point = Vector2(-visible_size.x/2 + camera.offset.x, position.y)
+	var end_point = Vector2(visible_size.x/2 + camera.offset.x, position.y)
+
+	points = [start_point, end_point]
