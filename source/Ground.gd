a81b9bbb7f3297030ff4d1ec53a708a966e3f0bc
Siphesihle Mhlongo <sihlecodes@gmail.com>
2023-01-12 19:50:02 +0200

    Fixed the y position clamp bug

--- source/Ground.gd
+++ source/Ground.gd
@@ -3,16 +3,13 @@ extends Line2D
 
 class_name Ground
 
-func _ready() -> void:
-	pass
-
 func _process(delta: float) -> void:
 	var camera = $"../camera"
 	var window_size = get_viewport_rect().size
 
 	var visible_size = window_size / camera.zoom
 
-	var start_point = Vector2(-visible_size.x/2 + camera.offset.x, position.y)
-	var end_point = Vector2(visible_size.x/2 + camera.offset.x, position.y)
+	var start_point = Vector2(-visible_size.x/2 + camera.offset.x, 0)
+	var end_point = Vector2(visible_size.x/2 + camera.offset.x, 0)
 
 	points = [start_point, end_point]
